8a130cc41907df262236e7b7a6dd4c52e320f823
unknown <ADNSU@itd-room-112-14.asoiu.local>
2019-10-17 19:41:00 +0400

    seh

---
+++ application/controllers/Pages.php
@@ -0,0 +1,36 @@
+<?php
+
+Class Pages extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+
+    public function about_history()
+{
+    $this->page = "about_history";
+    $this->page_pre = "History";
+    $this->load->view('includes/index');
+}
+
+    public function about_presences()
+    {
+        $this->page_pre = "Presences";
+        $this->page = "about_presences";
+        $this->load->view('includes/index');
+        
+    }
+
+    public function risk_management()
+    {
+        $this->page_pre = "Risk Management";
+        $this->page = "risk_management";
+        $this->load->view('includes/index');
+        
+    }
+}
+
