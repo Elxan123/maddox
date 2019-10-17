b42a75812eb0c0f4e7b3e2407bcf5c0f4944daf2
JavidLalayev <cavidleleyev9a12014@gmail.com>
2019-10-17 21:02:37 +0400

    Turalin contentleri

--- application/views/activity_trading.php
+++ application/views/activity_trading.php
@@ -1,1 +1,35 @@
 <?php $this->load->view("includes/preheader") ?>
+
+<div class="service-details-sec pt-100 pb-70">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="sidebar">
+
+
+
+
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="service-details-desc">
+
+                    <div class="service-details-text">
+                        <h2><a href="#">Trading</a></h2>
+                        <p style="font-size: 19px; font-family: Arial;">
+                            <br>   MADDOX  is one of the largest independent traders of crude oil and refined petroleum products in our  core region , the Caspian Sea, Black-Sea, Balkan, FSU, Baltic and Mediterranean region.
+                            <br>   Our TRADING activities in our core region is enhanced by our extensive logistical and storage capabilities.
+                            <br>   Based on our sourcing capabilities in our core region we are utilizing arbitrage opportunities to supply Crude Oil and Refined Petroleum Products to North America, north Africa, AG & PG as well as Asian markets.
+                            <br>   Our aim is to be close to our clients and to understand their daily needs to ensure a smooth and sound delivery process in line with our high international standards of operation.
+                            <br>   Our target is to establish and maintain long term relationships with our counterparties along the value-chain from exploration until final consumption.
+                            <br>   To achieve these goals, we are working with dedicated partners who follow our approach of best practice in every aspect of HSSE and with infrastructure from Depots, transport logistics, Terminals, and vessels that comply with the standards of MADDOX and our first class insurers.
+
+
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
--- application/views/value_social.php
+++ application/views/value_social.php
@@ -1,1 +1,37 @@
 <?php $this->load->view("includes/preheader") ?>
+
+<div class="service-details-sec pt-100 pb-70">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="sidebar">
+
+
+
+
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="service-details-desc">
+
+                    <div class="service-details-text">
+                        <h2><a href="#">Social Responsiblity</a></h2>
+                        <p style="font-size: 19px; font-family: Arial;">
+                            Our Employee
+                            <br> <br>
+                            We have created a diverse and international culture and promote teamwork, mutual respect and excellence. We focus on the overall well-being of our staff ensuring a nice working environment and supportive atmosphere as well as continuous development.
+                            <br> We, for example, accommodate, finance and support a few Masters in Trading Shipping and Finance, as well as CFC Trainee.
+                            <br>  We want to be fair and we assist our teams in growing both personally and professionally.
+                            <br>  We comply with all regulations in the country we operate in.
+
+                            <br> <br>  Our counterparties
+                            <br> <br>
+                            We believe in creating long-term relationship with our counterparties. This goes through treating all our relationships with fairness and providing support to our clients in creating economic success within the respect of the communities they operate in. We always work  towards delivering on what we promise.
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
